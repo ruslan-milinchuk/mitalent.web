12f0f9e516d4d0a741f2feb16c053a14c2c1431f
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-09-20 11:47:59 +0300

    feat: add fonts

--- src/App.js
+++ src/App.js
@@ -9,21 +9,15 @@ import HomePage from "./pages/HomePage";
 import Header from "./pages/Header";
 const history = createBrowserHistory();
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Router history={history}>
-          <Header />
-          <Route exact path="/" component={HomePage} />
-          <Route path="/aboute" component={About} />
-          <Route path="/clients" component={Clients} />
-          <Route path="/news" component={News} />
-          <Route path="/contact-us" component={ContactUs} />
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router history={history}>
+    <Header />
+    <Route exact path="/" component={HomePage} />
+    <Route path="/aboute" component={About} />
+    <Route path="/clients" component={Clients} />
+    <Route path="/news" component={News} />
+    <Route path="/contact-us" component={ContactUs} />
+  </Router>
+);
 
 export default App;
