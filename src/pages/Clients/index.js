f95eac2312ff5256a4a7341a8dd1c03a9f9669b1
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-09-21 16:50:32 +0300

    feat: add style to ProfileSlider

--- src/pages/Clients/index.js
+++ src/pages/Clients/index.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import ProfileSlider from "../../components/ProfileSlider";
 
 class Clients extends Component {
   render() {
+    const list = [
+      "https://images.pexels.com/photos/2963033/pexels-photo-2963033.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      "https://images.pexels.com/photos/2963032/pexels-photo-2963032.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      "https://images.pexels.com/photos/2963034/pexels-photo-2963034.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      "https://images.pexels.com/photos/2963035/pexels-photo-2963035.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+    ];
+
+    const name = ["Derec", "Anderson"];
+
+    const role = "American actor and comedian";
     return (
       <div>
-        <h1>Clients</h1>
+        <img
+          src="../../img/persons/NoemieBeier/photoPerson/photo2.jpeg"
+          alt=""
+        />
+        <ProfileSlider list={list} name={name} role={role} />
       </div>
     );
   }
