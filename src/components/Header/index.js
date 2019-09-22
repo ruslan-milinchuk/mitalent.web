acb6afc952c951fe68a12ea46b14af34fb626f39
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-09-22 13:06:45 +0300

    fix: use destructuring and logical operators, use varification to ButtonList, fix media

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,41 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
 import BurgerMenu from "../BurgerMenu";
 import { Link } from "react-router-dom";
 import "./style.css";
 import IconSearch from "../../icons/IconSearch";
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <div className="header__menu">
-          <BurgerMenu />
-          <Link className="header__item_fixed" to="/clients">
-            Clients
-          </Link>
-          <Link className="header__item_fixed" to="/news">
-            News
-          </Link>
-        </div>
-        <h1>
-          <Link className="header__logo" to="/">
-            <span>MI</span>
-            <span className="header__logo-spasing">TALENT</span>
-          </Link>
-        </h1>
-        <form className="header__search-form">
-          <input
-            className="header__input"
-            type="text"
-            placeholder="Search client"
-          />
-          <button className="header__button" type="submit">
-            <IconSearch />
-          </button>
-        </form>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__menu">
+        <BurgerMenu />
+        <Link className="header__item_fixed" to="/clients">
+          Clients
+        </Link>
+        <Link className="header__item_fixed" to="/news">
+          News
+        </Link>
       </div>
-    );
-  }
-}
+      <h1>
+        <Link className="header__logo" to="/">
+          <span>MI</span>
+          <span className="header__logo-spasing">TALENT</span>
+        </Link>
+      </h1>
+      <form className="header__search-form">
+        <input
+          className="header__input"
+          type="text"
+          placeholder="Search client"
+        />
+        <button className="header__button" type="submit">
+          <IconSearch />
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Header;
