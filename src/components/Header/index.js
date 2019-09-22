27aea0e3304331a8e73b6d3532f7e9cafdbb76d7
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-09-22 17:20:13 +0300

    fix: add Header position fixed and change clolrs

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,32 +6,34 @@ import IconSearch from "../../icons/IconSearch";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="header__menu">
-        <BurgerMenu />
-        <Link className="header__item_fixed" to="/clients">
-          Clients
-        </Link>
-        <Link className="header__item_fixed" to="/news">
-          News
-        </Link>
+    <div className="header__wrapp">
+      <div className="header">
+        <div className="header__menu">
+          <BurgerMenu />
+          <Link className="header__item_fixed" to="/clients">
+            Clients
+          </Link>
+          <Link className="header__item_fixed" to="/news">
+            News
+          </Link>
+        </div>
+        <h1>
+          <Link className="header__logo" to="/">
+            <span>MI</span>
+            <span className="header__logo-spasing">TALENT</span>
+          </Link>
+        </h1>
+        <form className="header__search-form">
+          <input
+            className="header__input"
+            type="text"
+            placeholder="Search client"
+          />
+          <button className="header__button" type="submit">
+            <IconSearch />
+          </button>
+        </form>
       </div>
-      <h1>
-        <Link className="header__logo" to="/">
-          <span>MI</span>
-          <span className="header__logo-spasing">TALENT</span>
-        </Link>
-      </h1>
-      <form className="header__search-form">
-        <input
-          className="header__input"
-          type="text"
-          placeholder="Search client"
-        />
-        <button className="header__button" type="submit">
-          <IconSearch />
-        </button>
-      </form>
     </div>
   );
 };
