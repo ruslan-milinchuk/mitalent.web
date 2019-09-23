8c978564bae39005d3ee47a9df9d834d30dfc921
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-09-23 13:39:37 +0300

    feat: add new components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Logo from "../Logo/Logo";
+import FormSearch from "../FormSearch";
+import Facebook from "../../icons/Facebook";
+import Twitter from "../../icons/Twitter";
+import Instagram from "../../icons/Instagram";
+import Youtube from "../../icons/Youtube";
+import "./style.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__section">
+        <Logo />
+        <FormSearch plaseholder="sign up our newsletter" icon="mail" />
+      </div>
+      <hr className="footer__divider" />
+      <div className="footer__section">
+        <p className="footer__date">
+          &copy; 2018 MI Talent.Designed by Tranmautritan for Mass Impressions
+        </p>
+        <div className="footer__buttons">
+          <a href="#">
+            <Facebook />
+          </a>
+          <a href="#">
+            <Twitter />
+          </a>
+          <a href="#">
+            <Instagram />
+          </a>
+          <a href="#">
+            <Youtube />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/FormSearch/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import IconSearch from "../../icons/IconSearch";
+import "./style.css";
+import IconMail from "../../icons/IconMail";
+const FormSearch = ({ plaseholder, icon }) => {
+  return (
+    <form className="search-form">
+      <input
+        className="search-form__input"
+        type="text"
+        placeholder={plaseholder}
+      />
+      <button className="search-form__button" type="submit">
+        {icon === "search" && <IconSearch />}
+        {icon === "mail" && <IconMail />}
+      </button>
+    </form>
+  );
+};
+
+export default FormSearch;
