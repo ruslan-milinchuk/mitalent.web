0e7071d36310b28e4e0591d9daadfca809f11ef6
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-09-23 20:48:14 +0300

    fix: del all unnecessary return

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,24 +5,22 @@ import "./style.css";
 import Logo from "../Logo/Logo";
 import FormSearch from "../FormSearch";
 
-const Header = () => {
-  return (
-    <div className="header__wrapp">
-      <div className="header">
-        <div className="header__menu">
-          <BurgerMenu />
-          <Link className="header__item_fixed" to="/clients">
-            Clients
-          </Link>
-          <Link className="header__item_fixed" to="/news">
-            News
-          </Link>
-        </div>
-        <Logo />
-        <FormSearch plaseholder="Search client" icon="search" />
+const Header = () => (
+  <div className="header__wrapp">
+    <div className="header">
+      <div className="header__menu">
+        <BurgerMenu />
+        <Link className="header__item_fixed" to="/clients">
+          Clients
+        </Link>
+        <Link className="header__item_fixed" to="/news">
+          News
+        </Link>
       </div>
+      <Logo />
+      <FormSearch placeholder="Search client" icon="search" />
     </div>
-  );
-};
+  </div>
+);
 
 export default Header;
--- src/components/LinksMenu/index.js
+++ src/components/LinksMenu/index.js
@@ -1,23 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const LinksMenu = ({ close }) => {
-  return (
-    <div className="header__list">
-      <Link onClick={close} className="header__item_unfixed" to="/about">
-        About
-      </Link>
-      <Link onClick={close} className="header__item_unfixed" to="/clients">
-        Clients
-      </Link>
-      <Link onClick={close} className="header__item_unfixed" to="/news">
-        News
-      </Link>
-      <Link onClick={close} className="header__item_unfixed" to="/contact-us">
-        Contact us
-      </Link>
-    </div>
-  );
-};
+const LinksMenu = ({ close }) => (
+  <div className="header__list">
+    <Link onClick={close} className="header__item_unfixed" to="/about">
+      About
+    </Link>
+    <Link onClick={close} className="header__item_unfixed" to="/clients">
+      Clients
+    </Link>
+    <Link onClick={close} className="header__item_unfixed" to="/news">
+      News
+    </Link>
+    <Link onClick={close} className="header__item_unfixed" to="/contact-us">
+      Contact us
+    </Link>
+  </div>
+);
 
 export default LinksMenu;
