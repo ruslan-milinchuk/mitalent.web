db7d9709e0bf412deb71317e6ffe26b4366a8d0b
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-09-23 13:46:44 +0300

    fix: add Logo and FormSearch to Header, del old blocks with tags

--- src/components/FormSearch/index.js
+++ src/components/FormSearch/index.js
@@ -2,20 +2,18 @@ import React from "react";
 import IconSearch from "../../icons/IconSearch";
 import "./style.css";
 import IconMail from "../../icons/IconMail";
-const FormSearch = ({ plaseholder, icon }) => {
-  return (
-    <form className="search-form">
-      <input
-        className="search-form__input"
-        type="text"
-        placeholder={plaseholder}
-      />
-      <button className="search-form__button" type="submit">
-        {icon === "search" && <IconSearch />}
-        {icon === "mail" && <IconMail />}
-      </button>
-    </form>
-  );
-};
+const FormSearch = ({ plaseholder, icon }) => (
+  <form className="search-form">
+    <input
+      className="search-form__input"
+      type="text"
+      placeholder={plaseholder}
+    />
+    <button className="search-form__button" type="submit">
+      {icon === "search" && <IconSearch />}
+      {icon === "mail" && <IconMail />}
+    </button>
+  </form>
+);
 
 export default FormSearch;
