8f63d2dae25f9f3d8bc13f194e774059c1cedec1
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-09-25 18:11:08 +0300

    feat: create new component ButtonsSocial

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import Logo from "../Logo/Logo";
 import FormSearch from "../FormSearch";
-import Facebook from "../../icons/Facebook";
-import Twitter from "../../icons/Twitter";
-import Instagram from "../../icons/Instagram";
-import Youtube from "../../icons/Youtube";
 import "./style.css";
+import ButtonsSocial from "../ButtonsSocial";
 const Footer = () => (
   <div className="footer">
     <div className="footer__section">
@@ -17,20 +14,7 @@ const Footer = () => (
       <p className="footer__date">
         &copy; 2018 MI Talent.Designed by Tranmautritan for Mass Impressions
       </p>
-      <div className="footer__buttons">
-        <a href="#">
-          <Facebook />
-        </a>
-        <a href="#">
-          <Twitter />
-        </a>
-        <a href="#">
-          <Instagram />
-        </a>
-        <a href="#">
-          <Youtube />
-        </a>
-      </div>
+      <ButtonsSocial component="footer" />
     </div>
   </div>
 );
