138accf3e5e0b96e58d017b558f5434d1b1c92b3
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-09-26 23:24:49 +0300

    fix: add mainFoto to fixtures, change the transfer props

--- src/components/PersonCardList/index.js
+++ src/components/PersonCardList/index.js
@@ -4,11 +4,11 @@ import ArrowRight from "../../icons/ArrowRight";
 const PersonCardList = ({ data }) =>
   data.map((item, index) => (
     <div key={index} className="client__card">
-      <img src={item.img} alt="client image" />
+      <img src={item.mainFoto} alt="client image" />
       <h3 className="client__name">
-        {item.name[0]} {item.name[1]}
+        {item.firstName} {item.lastName}
       </h3>
-      <h4 className="client__role">{item.role}</h4>
+      <h4 className="client__role">{item.type}</h4>
       <a className="client__link" href={item.link}>
         <ArrowRight />
       </a>
