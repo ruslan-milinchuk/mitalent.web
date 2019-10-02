8816d2bbdf5aa099e3e96373f9926a60b02866af
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-02 14:02:09 +0300

    feat: add list articles to home-page, add media to them, del console.log

--- src/components/OurStoriesArticles/index.js
+++ src/components/OurStoriesArticles/index.js
@@ -4,7 +4,10 @@ import "./style.css";
 const OurStoriesArticles = ({ newArticles }) => {
   return newArticles.map(item => (
     <div className="our-stories__item">
-      <img className="our-stories__img" src={item.person.img} alt="" />
+      <div
+        className="our-stories__img"
+        style={{ backgroundImage: "url(" + item.person.img + ")" }}
+      />
       <div className="our-stories__info">
         <h3 className="our-stories__title">{item.title}</h3>
         <h3 className="our-stories__date">
