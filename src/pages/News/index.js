05b90abd565ffb2ef2d6b4a7fb4184571cbe3db8
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-03 23:23:55 +0300

    feat: change articles.js and OurStoriesArticles, add base for News

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -1,9 +1,71 @@
 import React, { Component } from "react";
+import OurStoriesArticles from "../../components/OurStoriesArticles";
+import articles from "../../fixtures/articles";
+import "./style.css";
+import ArrowLeft from "../../icons/ArrowLeft";
+import ArrowRight from "../../icons/ArrowRight";
 
 class News extends Component {
-  render() {
-    return <div></div>;
+  state = {
+    newArticles: [],
+    articlesLength: 0,
+    count: 0
+  };
+
+  componentDidMount() {
+    let newArticles = articles.slice(0, 11);
+    this.setState({
+      newArticles: newArticles,
+      articlesLength: articles.length
+    });
   }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    const { count } = this.state;
+    if (this.state.count < nextState.count) {
+      const newArticles = articles.slice(count + 1, count + 12);
+      return this.setState({ newArticles: newArticles });
+    }
+
+    if (this.state.count > nextState.count) {
+      const newArticles = articles.slice(count - 1, count + 10);
+      return this.setState({ newArticles: newArticles });
+    }
+
+    return this.state.count === nextState.count;
+  }
+
+  render() {
+    const { newArticles } = this.state;
+
+    return (
+      <div className="news">
+        <OurStoriesArticles newArticles={newArticles} className="news" />
+        <div className="news__control">
+          <div className="news__control-left" onClick={this.clickArrowLeft}>
+            <ArrowLeft />
+            <p className="news__control-name"> prev post</p>
+          </div>
+          <div className="news__control-right" onClick={this.clickArrowRight}>
+            <p className="news__control-name">next post </p>
+            <ArrowRight />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  clickArrowLeft = () => {
+    const { count } = this.state;
+    if (count - 1 >= 0) {
+      return this.setState({ count: count - 1 });
+    }
+  };
+  clickArrowRight = () => {
+    const { count, articlesLength } = this.state;
+    if (count + 11 < articlesLength) {
+      return this.setState({ count: count + 1 });
+    }
+  };
 }
 
 export default News;
