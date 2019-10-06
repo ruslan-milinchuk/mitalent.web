714cd11541d7ba380583100dc2d62e20c2678d73
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-06 18:55:10 +0300

    fix: del activ class from logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,22 +1,13 @@
-import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
-class Logo extends Component {
-  render() {
-    const { history } = this.props;
-    const { pathname } = history.location;
-    return (
-      <h1>
-        <Link
-          className={pathname === "/" ? "logo logo__active" : "logo"}
-          to="/"
-        >
-          <span>MI</span>
-          <span className="logo__spasing">TALENT</span>
-        </Link>
-      </h1>
-    );
-  }
-}
+const Logo = () => (
+  <h1>
+    <Link className="logo" to="/">
+      <span>MI</span>
+      <span className="logo__spasing">TALENT</span>
+    </Link>
+  </h1>
+);
 
-export default withRouter(Logo);
+export default Logo;
