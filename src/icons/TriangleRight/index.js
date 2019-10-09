c023abcc86234fde43b821f02a29b1f39d41996e
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-09 03:44:54 +0300

    feat: add random client to HomePage, add media to him, add icon TriangleRight

---
+++ src/icons/TriangleRight/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TriangleRight = () => (
+  <svg
+    version="1.1"
+    id="Capa_1"
+    x="0px"
+    y="0px"
+    viewBox="0 0 490 490"
+    fill="currentColor"
+  >
+    <path d="M472.981,245.008L17.019,0v490L472.981,245.008z M47.634,51.214l360.68,193.794L47.634,438.786V51.214z" />
+  </svg>
+);
+
+export default TriangleRight;
