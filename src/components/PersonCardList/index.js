965de791bec7f2e3dd9fb192230185fccc1cbd4a
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-11 16:23:02 +0300

    fix: add link to Profile in PersonCardList

--- src/components/PersonCardList/index.js
+++ src/components/PersonCardList/index.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+
 import ArrowRight from "../../icons/ArrowRight";
 
-const PersonCardList = ({ data }) =>
-  data.map((item, index) => (
-    <div key={index} className="client__card">
-      <img src={item.mainFoto} alt="client image" />
-      <h3 className="client__name">
-        {item.firstName} {item.lastName}
-      </h3>
-      <h4 className="client__role">{item.type}</h4>
-      <a className="client__link" href={item.link}>
-        <ArrowRight />
-      </a>
-    </div>
-  ));
+class PersonCardList extends Component {
+  render() {
+    const { data, history } = this.props;
+    return data.map((item, index) => (
+      <div key={index} className="client__card">
+        <img src={item.mainFoto} alt="client image" />
+        <h3 className="client__name">
+          {item.firstName} {item.lastName}
+        </h3>
+        <h4 className="client__role">{item.type}</h4>
+        <a
+          onClick={() => history.push(`/profile/${item.id}`)}
+          className="client__link"
+          href={item.link}
+        >
+          <ArrowRight />
+        </a>
+      </div>
+    ));
+  }
+}
 
-export default PersonCardList;
+export default withRouter(PersonCardList);
