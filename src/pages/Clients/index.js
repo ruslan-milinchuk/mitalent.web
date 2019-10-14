83a95ff09cf2cd8f9999a3660760da03915c4f25
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-14 19:08:01 +0300

    fix: delete unnecessary cod in Clients

--- src/pages/Clients/index.js
+++ src/pages/Clients/index.js
@@ -1,25 +1,8 @@
 import React, { Component } from "react";
-import ProfileSlider from "../../components/ProfileSlider";
 
 class Clients extends Component {
   render() {
-    const list = [
-      "./img/persons/NoemieBeier/personPhoto/photo-1.jpg",
-      "https://images.pexels.com/photos/2963032/pexels-photo-2963032.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      "https://images.pexels.com/photos/2963034/pexels-photo-2963034.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      "https://images.pexels.com/photos/2963035/pexels-photo-2963035.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-    ];
-
-    const link = "#";
-
-    const name = ["Derec", "Anderson"];
-
-    const role = "American actor and comedian";
-    return (
-      <div>
-        <ProfileSlider list={list} name={name} role={role} link={link} />
-      </div>
-    );
+    return <div>Clients</div>;
   }
 }
 
