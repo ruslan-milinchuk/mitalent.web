acade9a31851a89e2450e8f8827530d015d02d01
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-11-10 12:05:35 +0200

    refactor: Article => function component, use hooks

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,75 +1,87 @@
-import React, { Component } from "react";
-import articles from "../../fixtures/articles";
+import React, { useState, useEffect } from "react";
+
 import PersonShortInfo from "../../components/PersonShortInfo";
+import RandomArticle from "../../components/RandomArticle";
+import Loading from "../../components/Loading";
+
+import { isEmpty } from "../../utils/isEmpty";
+import apiFetch from "../../utils/apiFetch";
 
 import "./style.css";
-import RandomArticle from "../../components/RandomArticle";
-class Article extends Component {
 
-  render() {
-    const { history } = this.props;
-    const { pathname } = history.location;
-    const idArticle = pathname.split("/")[2];
-    const neededArticle = [];
-    articles.map(item => {
-      if (idArticle === item.id) {
-        neededArticle.push(item);
-      }
-    });
-    const {
-      title,
-      typeArticle,
-      shortDescription,
-      slider,
-      quote,
-      longDescription,
-      additionalTitle,
-      additionalLongDescription
-    } = neededArticle[0];
-    const allQuote = quote.map((item, index) => <p key={index}>{item}</p>);
-    const { person } = neededArticle[0];
-    const { profileId } = person;
-    return (
-      <div className="article-info">
-        <div className="article-info__main">
-          <PersonShortInfo idPerson={profileId} />
-          <div className="article-info__main-info">
-            <h3 className="article-info__title">{title}</h3>
-            <div className="person__short-info_none"><PersonShortInfo idPerson={profileId} /></div>
-            <p className="article-info__type-article">{typeArticle[0]}</p>
-            <p className="article-info__subtitle">{shortDescription}</p>
-          </div>
-        </div>
-        <div className="article-info__first-img_hidden">
-          <img className="article-info__first-img" src={slider[0]} alt="" />
-        </div>
-        <div className="article-info__short-description-wrapp">
-          <PersonShortInfo idPerson={profileId} />
-          <div className="article-info__short-description">
-            <div className="article-info__quote">{allQuote}</div>
-            <div className="article-info__description">{longDescription}</div>
-          </div>
-        </div>
-        <div className="article-info__second-img">
-          <div
-            className="article-info__second-img-paramount"
-            style={{ backgroundImage: "url(" + slider[1] + ")" }}
-          />
-          <div
-            className="article-info__second-img-minor"
-            style={{ backgroundImage: "url(" + slider[2] + ")" }}
-          />
-        </div>
-        <div className="article-info__additional-info">
-          <h4 className="article-info__additional-title">{additionalTitle}</h4>
-          <div className="article-info__additional-description">
-            {additionalLongDescription}
-          </div>
-        </div>
-        <RandomArticle articles={articles} newArticleLength={4} />
-      </div>
-    );
+const Article = ({ history }) => {
+  const [article, setArticle] = useState({});
+  const { pathname } = history.location;
+  const idArticle = pathname.split("/")[2];
+  useEffect(() => {
+    const callData = async () => {
+      const [article] = await apiFetch(`/articles?uuid=${idArticle}`);
+      setArticle(article);
+    };
+    callData();
+  }, [idArticle]);
+  if (isEmpty(article)) {
+    return <Loading />;
   }
-}
+  const {
+    title,
+    typeArticle,
+    shortDescription,
+    slider,
+    quote,
+    longDescription,
+    additionalTitle,
+    additionalLongDescription
+  } = article;
+
+  const { person } = article;
+  const { profileId } = person;
+  return (
+    <div className="article-info">
+      <div className="article-info__main">
+        <PersonShortInfo idPerson={profileId} />
+        <div className="article-info__main-info">
+          <h3 className="article-info__title">{title}</h3>
+          <div className="person__short-info_none">
+            <PersonShortInfo idPerson={profileId} />
+          </div>
+          <p className="article-info__type-article">{typeArticle[0]}</p>
+          <p className="article-info__subtitle">{shortDescription}</p>
+        </div>
+      </div>
+      <div className="article-info__first-img_hidden">
+        <img className="article-info__first-img" src={slider[0]} alt="" />
+      </div>
+      <div className="article-info__short-description-wrapp">
+        <PersonShortInfo idPerson={profileId} />
+        <div className="article-info__short-description">
+          <div className="article-info__quote">
+            {quote.map((item, index) => (
+              <p key={index}>{item}</p>
+            ))}
+          </div>
+          <div className="article-info__description">{longDescription}</div>
+        </div>
+      </div>
+      <div className="article-info__second-img">
+        <div
+          className="article-info__second-img-paramount"
+          style={{ backgroundImage: "url(" + slider[1] + ")" }}
+        />
+        <div
+          className="article-info__second-img-minor"
+          style={{ backgroundImage: "url(" + slider[2] + ")" }}
+        />
+      </div>
+      <div className="article-info__additional-info">
+        <h4 className="article-info__additional-title">{additionalTitle}</h4>
+        <div className="article-info__additional-description">
+          {additionalLongDescription}
+        </div>
+      </div>
+      <RandomArticle newArticleLength={4} />
+    </div>
+  );
+};
 
 export default Article;
