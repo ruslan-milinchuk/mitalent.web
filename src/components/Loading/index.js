cd10d84e5b78cd7c6f761e865d3fd3f826050303
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-11-10 11:15:56 +0200

    feat: add directory utils with functions isEmpty and apiFetch, add component Loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import styles from "./loading.module.css";
+
+const Loading = () => (
+  <div className={styles.skFadingCircle}>
+    <div className={`${styles.skCircle1} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle2} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle3} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle4} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle5} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle6} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle7} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle8} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle9} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle10} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle11} ${styles.skCircle}`} />
+    <div className={`${styles.skCircle12} ${styles.skCircle}`} />
+  </div>
+);
+
+export default Loading;
