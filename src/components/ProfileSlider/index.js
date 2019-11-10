959d445babe6755d2301a7f1853ed1fdb3be4779
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-11-10 20:51:23 +0200

    refactor: do ProfileSlider function component, use hooks, delete useless code, fix style

--- src/components/ProfileSlider/index.js
+++ src/components/ProfileSlider/index.js
@@ -1,105 +1,85 @@
-import React, { Component } from "react";
-import "./style.css";
+import React, { useEffect, useState } from "react";
+
+import ButtonList from "../ButtonList";
+import Loading from "../Loading";
+
 import ArrowLeft from "../../icons/ArrowLeft";
 import ArrowRight from "../../icons/ArrowRight";
-import ButtonList from "../ButtonList";
-import persons from "../../fixtures/persons";
 
-class ProfileSlider extends Component {
-  state = { currentIndex: 0 };
+import apiFetch from "../../utils/apiFetch";
+import { isEmpty } from "../../utils/isEmpty";
 
-  render() {
-    const { currentIndex } = this.state;
-    const { idPerson } = this.props;
-    let neededPerson = persons.filter(item => idPerson === item.id)[0];
-    const {
-      firstName,
-      lastName,
-      type,
-      mainPhoto,
-      profilePhoto,
-      pressPhoto
-    } = neededPerson;
-    let list = [];
-    list.push(mainPhoto, profilePhoto, pressPhoto);
-    let role = type;
-    const link = "#";
-    return (
-      <div className="person">
-        <div className="person__content">
-          <div className="person__name">
-            <h2 className="person__name_font">
-              {firstName}&nbsp;
-              <br className="person__br-none" />
-              {lastName}
-            </h2>
-          </div>
-          <h3 className="person__role">{role}</h3>
-          <div className="person__btn">
-            <div className="person__btn-list">
-              <ButtonList
-                currentIndex={currentIndex}
-                list={list}
-                stateNumb={this.stateNumb}
-              />
-            </div>
-            <div className="person__btn-arrow_disp-inline">
-              <button
-                onClick={() => this.slideLeft(list)}
-                className="person__btn-left"
-              >
-                <ArrowLeft />
-              </button>
-              <button
-                onClick={() => this.slideRight(list)}
-                className="person__btn-right"
-              >
-                <ArrowRight />
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="person__img">
-          <img src={`.${list[currentIndex]}`} alt="" />
-          <a className="person__link" href="#contact">
-            Contact {firstName}
-          </a>
-        </div>
-        <div className="person__btn-arrow_disp-none">
-          <button
-            onClick={() => this.slideLeft(list)}
-            className="person__btn-left"
-          >
-            <ArrowLeft />
-          </button>
-          <button
-            onClick={() => this.slideRight(list)}
-            className="person__btn-right"
-          >
-            <ArrowRight />
-          </button>
-        </div>
-      </div>
-    );
+import "./style.css";
+
+const ProfileSlider = ({ idPerson }) => {
+  const [index, setIndex] = useState(0);
+  const [person, setPerson] = useState({});
+  useEffect(() => {
+    const callData = async () => {
+      const [person] = await apiFetch(`/people?uuid=${idPerson}`);
+      setPerson(person);
+    };
+    callData();
+  }, [idPerson]);
+  if (isEmpty(person)) {
+    return <Loading />;
   }
 
-  slideLeft = list => {
-    let { currentIndex } = this.state;
+  const {
+    firstName,
+    lastName,
+    type = [],
+    mainPhoto,
+    profilePhoto,
+    pressPhoto
+  } = person;
+  let list = [mainPhoto, profilePhoto, pressPhoto];
+  return (
+    <div className="person">
+      <div className="person__content">
+        <div className="person__name">
+          <h2 className="person__name_font">
+            {firstName}&nbsp;
+            <br className="person__br-none" />
+            {lastName}
+          </h2>
+        </div>
+        <h3 className="person__role">{type[0]}</h3>
+        <ButtonSlideControl setIndex={setIndex} index={index} list={list} />
+      </div>
+      <div className="person__img">
+        <img src={`.${list[index]}`} alt="" />
+        <a className="person__link" href="#contact">
+          Contact {firstName}
+        </a>
+      </div>
+    </div>
+  );
+};
 
-    currentIndex === 0
-      ? this.setState({ currentIndex: list.length - 1 })
-      : this.setState({ currentIndex: currentIndex - 1 });
-  };
-  stateNumb = index => {
-    return this.setState({ currentIndex: index });
-  };
-
-  slideRight = list => {
-    let { currentIndex } = this.state;
-
-    currentIndex === list.length - 1
-      ? this.setState({ currentIndex: 0 })
-      : this.setState({ currentIndex: currentIndex + 1 });
-  };
-}
+const ButtonSlideControl = ({ setIndex, index, list }) => (
+  <div className="person__btn">
+    <div className="person__btn-list">
+      <ButtonList currentIndex={index} list={list} setIndex={setIndex} />
+    </div>
+    <div className="person__btn-arrow_disp-inline">
+      <button
+        onClick={() =>
+          index === 0 ? setIndex(list.length - 1) : setIndex(index - 1)
+        }
+        className="person__btn-left"
+      >
+        <ArrowLeft />
+      </button>
+      <button
+        onClick={() =>
+          index === list.length - 1 ? setIndex(0) : setIndex(index + 1)
+        }
+        className="person__btn-right"
+      >
+        <ArrowRight />
+      </button>
+    </div>
+  </div>
+);
 export default ProfileSlider;
