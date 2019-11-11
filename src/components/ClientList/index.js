e09c27a5db279bbdc4af35e5d9583d24b1dd7de4
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-11-11 11:08:06 +0200

    refactor: ClientList to function component, fix scss

--- src/components/ClientList/index.js
+++ src/components/ClientList/index.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
+
 import PersonCardList from "../PersonCardList";
+
 import "./style.css";
 
-class ClientList extends Component {
-  render() {
-    const { startPath, data } = this.props;
-    return (
-      <div className="client__list">
-        <PersonCardList data={data} startPath={startPath} />
-      </div>
-    );
-  }
-}
+const ClientList = ({ startPath, data }) => (
+  <div className="client__list">
+    <PersonCardList data={data} startPath={startPath} />
+  </div>
+);
+
 export default ClientList;
