4a73780636e45a7847bfa02b0d8261dc2571b40a
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-11-11 14:17:56 +0200

    refactor: components without style.scss move to files

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 import classNames from "classnames";
-import {formatterIndex} from "../../utils/formatterIndex";
+import { formatterIndex } from "../../utils/formatterIndex";
 
 const ButtonList = ({ currentIndex, list, setIndex }) =>
   list.map((item, index) => (
     <button
       key={index}
       onClick={() => setIndex(index)}
-      className={classNames("person__btn-item",
-        { "person__btn-item-active": currentIndex === index },
-      )}
+      className={classNames("person__btn-item", {
+        "person__btn-item-active": currentIndex === index
+      })}
     >
       {formatterIndex(index)}
     </button>
